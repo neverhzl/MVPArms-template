807b59738ccf8d2fe9dfa4af4b37d57fd311442f
GitHub Action <action@github.com>
2022-03-27 11:18:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/neverhzl/mvparmstemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/neverhzl/mvparmstemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.neverhzl.mvparmstemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.neverhzl.mvparmstemplate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/neverhzl/mvparmstemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/neverhzl/mvparmstemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.neverhzl.mvparmstemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.neverhzl.mvparmstemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
